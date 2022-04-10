3c0dc19e2c87ec246e7964ac90bfd7fca000e64e
adrian <adrianpasternak@gmail.com>
2022-04-10 21:36:23 +0200

    fix formatting

--- recipe_scrapers/finedininglovers.py
+++ recipe_scrapers/finedininglovers.py
@@ -26,27 +26,7 @@ class FineDiningLovers(AbstractScraper):
         return self.schema.ingredients()
 
     def instructions(self):
-<<<<<<< HEAD
         return self.schema.instructions()
-=======
-        instructions_parent = self.soup.find(
-            "div", {"class": "field--name-field-recipe-para-steps"}
-        )
-
-        if instructions_parent is not None:
-            instructions = instructions_parent.findAll(
-                "div", {"class": "paragraph--type--recipe-step"}
-            )
-        else:
-            instructions_parent = self.soup.find(
-                "div", {"class": "ante-body"}
-            )
-            instructions = instructions_parent.findAll({"li", "p"})
-
-        return "\n".join(
-            [normalize_string(instruction.get_text()) for instruction in instructions]
-        )
->>>>>>> fix finedininglovers.com instruction parsing
 
     def image(self):
         return self.schema.image()
